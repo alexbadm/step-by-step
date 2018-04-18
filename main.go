d5238006116299cbe5c11f6da99bbdcdb27aa442
Alex B <eto.ya@list.ru>
2018-04-18 15:48:04 +0300

    Added glide and env variable

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/takama/router"
 )
@@ -10,8 +12,10 @@ var log = logrus.New()
 // Run server: go build && step-by-step
 // Try requests: curl http://127.0.0.1:8000/test
 func main() {
+	port := os.Getenv("SERVICE_PORT")
+
 	r := router.New()
 	r.Logger = logger
 	r.GET("/", home)
-	r.Listen(":8000")
+	r.Listen(":" + port)
 }
